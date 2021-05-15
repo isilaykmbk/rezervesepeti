2929bb7ce3bf75ed67b040951e98816966d58389
oguzmuay <69862688+oguzmuay@users.noreply.github.com>
2021-05-15 03:02:05 +0300

    Giris ekrani java kodlari eklendi.

--- app/src/main/java/com/rezerve_sepeti/MainActivity.java
+++ app/src/main/java/com/rezerve_sepeti/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.rezerve_sepeti.businessPart.SignInActivity;
 
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, SignInActivity.class));
+        findViewById(R.id.businessbutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignInActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/rezerve_sepeti/userPart/UserSignInActivity.java
@@ -0,0 +1,16 @@
+package com.rezerve_sepeti.userPart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rezerve_sepeti.R;
+
+public class UserSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}

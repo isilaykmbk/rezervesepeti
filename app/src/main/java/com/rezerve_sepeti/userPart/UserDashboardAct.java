ec68c97b9f0f6b549f2bd899171b1d7857ea2ebd
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2021-05-18 13:39:28 +0300

    User ' a sign out eklendi... Eksik olan bar 'ı ekledik...

--- app/src/main/java/com/rezerve_sepeti/userPart/UserDashboardAct.java
+++ app/src/main/java/com/rezerve_sepeti/userPart/UserDashboardAct.java
@@ -3,24 +3,38 @@ package com.rezerve_sepeti.userPart;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.rezerve_sepeti.R;
 
 public class UserDashboardAct extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.rezerv_options_menu,menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.usersignout){
+            firebaseAuth.signOut();
+            Intent intentUserToSignUp = new Intent(UserDashboardAct.this, UserSignUpActivity.class);
+            startActivity(intentUserToSignUp);
+        }
         return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +42,5 @@ public class UserDashboardAct extends AppCompatActivity {
         setContentView(R.layout.activity_user_dashboard);
     }
 
-
 }
+

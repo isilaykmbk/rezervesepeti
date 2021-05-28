b3b4effa8d1dab2b1da5a6daa3a1d8206392a20c
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2021-05-28 20:15:56 +0300

    LongClick bolumu eklendi...

---
+++ app/src/main/java/com/rezerve_sepeti/Place.java
@@ -0,0 +1,14 @@
+package com.rezerve_sepeti;
+
+public class Place {
+    private String name;
+    private Double latitude;
+    private Double longitude;
+
+    public Place(String name, Double latitude, Double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+}
--- app/src/main/java/com/rezerve_sepeti/UserMapsActivity.java
+++ app/src/main/java/com/rezerve_sepeti/UserMapsActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -26,7 +28,13 @@ import com.rezerve_sepeti.databinding.ActivityUserMapsBinding;
 import com.rezerve_sepeti.userPart.UserDashboardAct;
 import com.rezerve_sepeti.userPart.UserSignInActivity;
 
-public class UserMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class UserMapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     LocationManager locationManager;            //1-0
@@ -56,18 +64,19 @@ public class UserMapsActivity extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener((GoogleMap.OnMapLongClickListener) this);
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE); //1-1 alt yerler dahil
         locationListener = location -> {
                 //kullanici istedigi yerde gezinebiliyor,çıkınca eski yerde kalıyor //3-0
             SharedPreferences sharedPreferences = UserMapsActivity.this.getSharedPreferences("com.rezerve_sepeti.rezervesepeti",MODE_PRIVATE);
             boolean trackBoolean = sharedPreferences.getBoolean("trackBoolean",false);
-
+            LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude()); //5-1
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(userLocation).title("Konumum"));
             //kaydedilmemisse true yap //3-1
              if(!trackBoolean) {
-                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude()); //5-1
                  mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));   //5-1
-                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Konumum"));
                  sharedPreferences.edit().putBoolean("trackBoolean",true).apply();
                 }
              };
@@ -94,7 +103,6 @@ public class UserMapsActivity extends FragmentActivity implements OnMapReadyCall
 
         }*/
 
-
     @Override //izin ilk defa verildiginde ne yapilacak 1-3
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -104,7 +112,6 @@ public class UserMapsActivity extends FragmentActivity implements OnMapReadyCall
                     //izin verildiyse 1-4
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-
                     // 2-1
 
                     Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -112,12 +119,48 @@ public class UserMapsActivity extends FragmentActivity implements OnMapReadyCall
                        LatLng lastUserLocation = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15));
                     }
-
                     /*else {
 
                     }*/
                 }
             }
         }
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull @NotNull LatLng latLng) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address = "";
+
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+
+            if(addressList != null && addressList.size() > 0) {
+                if(addressList.get(0).getThoroughfare() != null) {
+                    address += addressList.get(0).getThoroughfare();
+
+                    if (addressList.get(0).getSubThoroughfare() != null) {
+                        address += " ";
+                        address += addressList.get(0).getSubThoroughfare();
+                    }
+                }
+            } else {
+                address = "New Place";
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Restoran"));
+
+        Double latitude = latLng.latitude;
+        Double longitude = latLng.longitude;
+
+        Place place = new Place(address, latitude, longitude);
+        System.out.println(address); // !!! Logcat bolumunu acip haritaya uzun basarsaniz adres gozukuyor
+
     }
 }

4a18493e31e816b8f92f9915de9027405fe62757
Oğuz Uğurdoğan <69862688+oguzmuay@users.noreply.github.com>
2021-05-29 00:10:56 +0300

    BUG VARMIS ALOOOOO
    KIMSEDE DEMIYO!!!

--- app/src/main/java/com/rezerve_sepeti/MainActivity.java
+++ app/src/main/java/com/rezerve_sepeti/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this, BusinessMapsActivity.class));
+        //startActivity(new Intent(MainActivity.this, BusinessMapsActivity.class));
         findViewById(R.id.businessbutton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
